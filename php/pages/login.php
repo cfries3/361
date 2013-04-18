bca20ed523c2ccb4d0e211ccffa50fefba314719
cfries3 <christine.friesen@mail.mcgill.ca>
2013-04-17 21:33:33 -0400

    login page changed

--- php/pages/login.php
+++ php/pages/login.php
@@ -1,6 +1,5 @@
 <?php
 
-
 ?>
 
 <html>
@@ -8,15 +7,33 @@
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>LoginPage</title>
 <script type="text/javascript" src="jsSHA-master\jsSHA-master\src\sha.js"></script>
-<link rel="stylesheet" type="text/css" href="./../../css/login.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="./../../css/default.css" />
+	<link rel="stylesheet" type="text/css" href="./../../css/formBased.css" />
+	
 </head>
 <body>
 
-<form action="./../loginScript.php" method="post" class="form">
-Username<input type="text" name="Username" class="field"/><br>
-Password <input type="password" name="Password" class="field" /><br>
-<div class="buttonPos"> <input type="submit" value="Login"/> </div>
-</form>
-
+	<div id="login">
+		<div id="miniBanner">
+			<img class="floatLeft" src="./../../images/logo.png" alt="Company Name" />
+			<h1>Business Portal</h1>
+		</div>
+		<form action="./../loginScript.php" method="post">
+			<table class="forms formsSM">
+				<tr>
+					<td class="col1"><label for="Username">Username</label></td>
+					<td class="col2"><input type="text" name="Username" class="field"/></td>
+				</tr>
+				<tr>
+					<td class="col1"><label for="password">Password</label></td>
+					<td class="col2"><input type="password" name="Password" class="field" /></td>
+				</tr>
+				<tr>
+					<td colspan="2" class="twoCol"><input type="submit" class="button" value="Login"/></td>
+				</tr>
+			</table>
+		</form>
+	</div>
+	
 </body>
 </html>
