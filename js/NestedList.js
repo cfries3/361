549407e12e8114c393bee7f3e1dd7768039e40eb
cfries3 <christine.friesen@mail.mcgill.ca>
2013-04-18 18:35:44 -0400

    cleaning up files

--- js/NestedList.js
+++ js/NestedList.js
@@ -2,11 +2,6 @@ $(document).ready(function(){
 	$.easing.def = "easeOutBounce";
 	$('li.clickable a').click(function(e) {
 		var dropdown = $(this).parent().next();
-/*		if (dropdown.className.match(/\btask\b/)) {
-			$('.dropdown').not(dropdown, '.project').slideUp('slow');
-		} else {
-			$('.dropdown').not(dropdown).slideUp('slow');
-		}*/
 		dropdown.slideToggle('slow');
 		e.preventDefault();
 	});
